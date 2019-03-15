af3a9e39d71fe79e98775470fa97514d4a169c9f
jcvdav <juancarlos.villader@gmail.com>
2019-03-15 14:01:06 -0700

    Added script that downloads the data

---
+++ scripts/download_monthly_foreign_fishing.R
@@ -0,0 +1,35 @@
+#########################################
+#   download_monthly_foreign_fishing    #
+#########################################
+
+
+#########################################
+# This script collects the data from GBQ.
+#########################################
+
+# Load packages
+library(startR)
+library(lubridate)
+library(here)
+library(tidyverse)
+
+# Get the table
+monthly_foreign_fishing <- get_table(project = "ucsb-gfw",
+                                     dataset = "foreign_fishing_ren",
+                                     table = "monthly_foreign_fishing") %>% 
+  mutate(date = date(paste(year, month, 1, sep = "-"))) %>% # Create a column with date
+  select(year,
+         month,
+         date,
+         eez_iso3,
+         country,
+         hours_by_self,
+         hours_by_foreign,
+         total_hours)
+
+# Export the data
+saveRDS(object = monthly_foreign_fishing,
+        file = here("raw_data", "monthly_foreign_fishing.rds")) 
+
+
+# END OF SCRIPT
